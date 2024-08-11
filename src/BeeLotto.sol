74aac3d2b70f7e1c85df5ee4e518c3720720933a
t9fiction <sohail.sohailishaq@gmail.com>
2024-08-12 01:43:36 +0500

    chainlink Automation done, on smart contract

--- src/BeeLotto.sol
+++ src/BeeLotto.sol
@@ -7,11 +7,10 @@ interface IERC20 {
 }
 
 contract BeeLotto {
-
     address public owner;
-    uint256 public ticketPrice = 10 * 10**18; // 50 Bee tokens per ticket
+    uint256 public ticketPrice = 10 * 10 ** 18; // 50 Bee tokens per ticket
     uint256 public maxTickets = 100; // maximum tickets per lottery
-    uint256 public ticketCommission = 50 * 10**16; // commission per ticket in Token
+    uint256 public ticketCommission = 50 * 10 ** 16; // commission per ticket in Token
     uint256 public maxTicketsPerWallet = 15;
     uint256 public lottoDuration = 1440 minutes; // The duration set for the lottery 1 Day
 
@@ -25,13 +24,12 @@ contract BeeLotto {
     IERC20 public token; // ERC20 token contract
     address public erc20Token = 0xF53D495fC33cb402D005F77C08c203A690C62b46;
 
-
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
         token = IERC20(erc20Token);
     }
@@ -47,7 +45,7 @@ contract BeeLotto {
         owner = newOwner;
     }
 
-    function setLottoTimer(uint256 _timeInMins) external onlyOwner returns(bool) {
+    function setLottoTimer(uint256 _timeInMins) external onlyOwner returns (bool) {
         // Get the current block timestamp
         require(_timeInMins * 1 minutes >= 5 minutes, "Lotto Duration can't be less then 5 minutes");
         uint256 currentTimestamp = block.timestamp;
@@ -56,7 +54,7 @@ contract BeeLotto {
         return true;
     }
 
-    function returnLottoTimer() external view returns(uint256){
+    function returnLottoTimer() external view returns (uint256) {
         return lottoDuration;
     }
 
@@ -70,11 +68,7 @@ contract BeeLotto {
     }
 
     function BuyTickets(uint256 _numOfTicketsToBuy) public {
-        
-        require(
-            _numOfTicketsToBuy <= RemainingTickets(),
-            "Not enough tickets available."
-        );
+        require(_numOfTicketsToBuy <= RemainingTickets(), "Not enough tickets available.");
 
         token.transferFrom(msg.sender, address(this), ticketPrice * _numOfTicketsToBuy);
 
@@ -87,9 +81,7 @@ contract BeeLotto {
         require(ticketHolders.length > 0, "No tickets were purchased");
 
         bytes32 blockHash = blockhash(block.number - ticketHolders.length);
-        uint256 randomNumber = uint256(
-            keccak256(abi.encodePacked(block.prevrandao, block.timestamp, blockHash))
-        );
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, blockHash)));
         uint256 winningTicket = randomNumber % ticketHolders.length;
 
         address winner = ticketHolders[winningTicket];
@@ -100,7 +92,6 @@ contract BeeLotto {
         delete ticketHolders;
     }
 
-    
     function checkWinningsAmount() public view returns (uint256) {
         address winner = msg.sender;
 
@@ -119,7 +110,6 @@ contract BeeLotto {
     }
 
     function RefundAll() public onlyOwner {
-
         for (uint256 i = 0; i < ticketHolders.length; i++) {
             address to = ticketHolders[i];
             ticketHolders[i] = address(0);
@@ -149,23 +139,24 @@ contract BeeLotto {
         return winningAmount[msg.sender] > 0;
     }
 
-
     //Function to change the price of TicketPrice
-    function updateTicketPrice(uint256 _tokensPerTicket) public onlyOwner returns(bool){
-        ticketPrice = _tokensPerTicket * 10**18;
+    function updateTicketPrice(uint256 _tokensPerTicket) public onlyOwner returns (bool) {
+        ticketPrice = _tokensPerTicket * 10 ** 18;
         return true;
     }
 
     //Function to change the Max number of tickets for each lotto
-    function updateMaxTickets(uint256 _maxTickets) public onlyOwner returns(bool){
+    function updateMaxTickets(uint256 _maxTickets) public onlyOwner returns (bool) {
         maxTickets = _maxTickets;
         return true;
     }
 
     //Function to update the Ticket commission for each ticket
-    function updateTicketCommission(uint256 _noOfTokensAsCommission) public onlyOwner returns(bool){
-        require(_noOfTokensAsCommission * 10**16 < ticketPrice,"Commission can't be greater then the Price of the Ticket");
-        ticketCommission = _noOfTokensAsCommission * 10**16;
+    function updateTicketCommission(uint256 _noOfTokensAsCommission) public onlyOwner returns (bool) {
+        require(
+            _noOfTokensAsCommission * 10 ** 16 < ticketPrice, "Commission can't be greater then the Price of the Ticket"
+        );
+        ticketCommission = _noOfTokensAsCommission * 10 ** 16;
         return true;
-    }    
+    }
 }
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,30 +1,153 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
- **
+ *
  */
-contract Raffle {
-    /** Custom Errors */
-    error NotEnoughEthSent();
+contract Raffle is VRFConsumerBaseV2Plus {
+    /**
+     * Custom Errors
+     */
+    error Raffle__NotEnoughEthSent();
+    error Raffle__NotEnoughTimePassed();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState);
 
-    uint256 private immutable i_entranceFee;
-
-    constructor(uint256 _entranceFee) {
-        i_entranceFee = _entranceFee;
+    /**
+    ** ENUMS / Type Declarations
+     */
+    enum RaffleState{
+        OPEN,
+        CALCULATING
     }
 
+
+    /**
+     * State Variables
+     */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
+    uint256 private s_lastTimestamp;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+
+    // bool s_calculatingWinner = false;
+
+    /**
+     * Events
+     */
+    event RaffleEnter(address indexed player);
+    event WinnerPicked(address indexed player);
+
+    /**
+     * Constructor
+     */
+    constructor(
+        uint256 _entranceFee,
+        uint256 _interval,
+        address _vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimestamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    //Entering into the Raffle. with minimum fees
     function enterRaffle() public payable {
         // require(msg.value >= i_entranceFee,"Not enough Eth sent!");
         // require(msg.value >= i_entranceFee, NotEnoughEthSent());
         if (msg.value < i_entranceFee) {
-            revert NotEnoughEthSent();
+            revert Raffle__NotEnoughEthSent();
+        }
+        if(s_raffleState != RaffleState.OPEN){
+            revert Raffle__RaffleNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    // Chainlink Automation logic trigger
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        returns (bool upkeepNeeded, bytes memory /* performData */)
+    {
+        bool timeHasPassed = ((block.timestamp - s_lastTimestamp) >= i_interval);
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+
+        upkeepNeeded = timeHasPassed && isOpen && hasBalance && hasPlayers;
+        return (upkeepNeeded, "");
+    }
+
+    function performUpkeep() public {
+        // Has enough time passed ?
+        (bool upkeepNeeded,) = checkUpkeep("");
+        if(!upkeepNeeded){
+            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
+        }
+        // if ((block.timestamp - s_lastTimestamp) < i_interval) {
+        //     revert Raffle__NotEnoughTimePassed();
+        // }
+
+        s_raffleState = RaffleState.CALCULATING;
+        // Get our random numbers
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+
+        s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    function fulfillRandomWords(
+        uint256 /* requestId */,
+        uint256[] calldata randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN; // Changing the state to open to let players in
+        s_players = new address payable[](0); //Resetting the array to empty
+        s_lastTimestamp = block.timestamp;
+        emit WinnerPicked(recentWinner);
+        (bool success,) = recentWinner.call{value:address(this).balance}("");
+        if(!success){
+            revert Raffle__TransferFailed();
         }
     }
-    function pickWinner() public {}
 
-    /** Getter Functions */
-
+    /**
+     * Getter Functions
+     */
     function getEntranceFees() public view returns (uint256) {
         return i_entranceFee;
     }
